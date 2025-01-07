9627c06aa55799261607e5f5c1350210e69788e8
sylvain33220 <sylvain.poteaux@gmail.com>
2025-01-07 17:47:44 +0100

    quete9

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -2,19 +2,128 @@
  namespace App\DataFixtures;
 
  use App\Entity\Program;
+ use App\Entity\Category;
  use Doctrine\Bundle\FixturesBundle\Fixture;
+ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
  use Doctrine\Persistence\ObjectManager;
 
- class ProgramFixtures extends Fixture
- {
+ class ProgramFixtures extends Fixture implements DependentFixtureInterface
+ {  
+    const PROGRAMS = [
+        [
+            'title' => 'Walking Dead',
+            'synopsis' => 'Des zombies envahissent la terre.',
+            'category' => 'category_Action',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 2',
+            'synopsis' => 'some texte',
+            'category' => 'category_Action',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 3',
+            'synopsis' => 'some texte',
+            'category' => 'category_Action',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 4',
+            'synopsis' => 'some texte',
+            'category' => 'category_Action',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 5',
+            'synopsis' => 'some texte',
+            'category' => 'category_Action',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'amelie poulain',
+            'synopsis' => 'Des zombies envahissent la terre.',
+            'category' => 'category_Aventure',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 6',
+            'synopsis' => 'some texte',
+            'category' => 'category_Aventure',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 7',
+            'synopsis' => 'some texte',
+            'category' => 'category_Aventure',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 8',
+            'synopsis' => 'some texte',
+            'category' => 'category_Aventure',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 9',
+            'synopsis' => 'some texte',
+            'category' => 'category_Aventure',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'starwars',
+            'synopsis' => 'Des zombies envahissent la terre.',
+            'category' => 'category_Fantastique',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 10',
+            'synopsis' => 'some texte',
+            'category' => 'category_Fantastique',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 11',
+            'synopsis' => 'some texte',
+            'category' => 'category_Fantastique',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 12',
+            'synopsis' => 'some texte',
+            'category' => 'category_Fantastique',
+            'poster' => '/',
+        ],
+        [
+            'title' => 'Titre 13',
+            'synopsis' => 'some texte',
+            'category' => 'category_Fantastique',
+            'poster' => '/',
+        ],
+     ];
     public function load(ObjectManager $manager):void
-    {
-        $program = new Program();
-        $program->setTitle('halloween');
-        $program->setSynopsis('un gros film d\'horreur');
-        $program->setPoster('/');
-        $manager->persist($program);
+    { 
+        foreach (self::PROGRAMS as $programData) {
+             /** @var array{
+     *     title: string,
+     *     synopsis: string,
+     *     category: string,
+     *     poster: string
+     * } $programData
+     */
+            $program = new Program();
+            $program->setTitle($programData['title']);
+            $program->setSynopsis($programData['synopsis']);
+            $program->setCategory($this->getReference($programData['category'],Category::class));
+            $program->setPoster($programData['poster']);
+            $manager->persist($program);
+        }
         $manager->flush();
-
+    }
+    public function getDependencies(): array
+    {
+        return [
+            CategoryFixtures::class,
+        ];
     }
  }
